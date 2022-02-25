5eead36f4308cad689eed9bf889601ec76abfd7d
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-02-25 14:58:30 +0900

    bubble Sort로 담첨번호 오름차순 정렬

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -74,7 +74,24 @@ public class MainDrive {
 //		winNumbers[4] = 20;
 //		winNumbers[5] = 31;
 
+//		랜덤으로 만들어진 당첨번호들을 > 작은수 ~ 큰 수로 정리
 		
+		for(int i=0; i<winNumbers.length; i++) {
+			
+			for(int j=0; j<winNumbers.length-1; j++) {
+				
+				if(winNumbers[j] > winNumbers[j+1]) {
+					
+					int backUp = winNumbers[j];
+					
+					winNumbers[j] = winNumbers[j+1];
+					
+					winNumbers[j+1] = backUp;
+				}
+			}
+		}
+		
+//		정리된 당첨 번호를 확인		
 		
 		for(int num: winNumbers) {
 			System.out.println(num);
